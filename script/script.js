e1fab7f0730c320413a74306f252a535d4ff1c88
Arafat Hossain Ar <arafat.122260@gmail.com>
2022-08-13 23:13:15 +0600

    transition added

--- script/script.js
+++ script/script.js
@@ -3,16 +3,16 @@ let i;
 
 for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
         this.classList.toggle("active");
 
         /* Toggle between hiding and showing the active panel */
         let body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
+        if (body.style.maxHeight) {
+            body.style.maxHeight = null;
         } else {
-            body.style.display = "block";
+            body.style.maxHeight = body.scrollHeight + "px";
         }
+
+
     });
 }
